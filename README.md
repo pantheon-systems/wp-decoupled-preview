e84d5a14363ef1563cfa61946167f16be824b797
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2022-09-13 16:21:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pantheon Decoupled Preview
+
+WordPress Decoupled preview plugin preview WordPress post or pages on Pantheon Front-End sites like NextJS, GatsbyJs etc.
---
+++ wp-decoupled-preview.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+ * Plugin Name:     Pantheon Decoupled Preview
+ * Plugin URI:      https://pantheon.io/
+ * Description:     Preview WordPress post or pages on Pantheon Front-End sites like NextJS, GatsbyJS etc.
+ * Version:         0.1.0
+ * Author:          Pantheon
+ * Author URI:      https://pantheon.io/
+ * Text Domain:     wp-decoupled-preview
+ * Domain Path:     /languages
+ *
+ * @package         Pantheon_Decoupled
+ */
