8a2c33d6c89cd6c9e5362807a94147b82d10bba8
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2022-10-19 18:55:14 +0530

    DB-3888: WordPress phpcs coding standards.

--- wp-decoupled-preview.php
+++ wp-decoupled-preview.php
@@ -12,24 +12,50 @@
  * @package         Pantheon_Decoupled
  */
 
-register_activation_hook(__FILE__, 'wp_decoupled_preview_default_options');
-register_deactivation_hook(__FILE__, 'wp_decoupled_preview_delete_default_options');
+register_activation_hook( __FILE__, 'wp_decoupled_preview_default_options' );
+register_deactivation_hook( __FILE__, 'wp_decoupled_preview_delete_default_options' );
 
+/**
+ * Set default values for the preview sites options.
+ *
+ * @return void
+ */
 function wp_decoupled_preview_default_options() {
-	add_option( 'preview_sites', ['preview' => [ 0 => [ 'label' => NULL, 'url' => NULL, 'secret_string' => NULL ] ] ] );
+	add_option(
+		'preview_sites',
+		[
+			'preview' => [
+				1 => [
+					'label'         => null,
+					'url'           => null,
+					'secret_string' => null,
+				],
+			],
+		]
+	);
 }
 
+/**
+ * Delete preview sites options when deactivation plugin.
+ *
+ * @return void
+ */
 function wp_decoupled_preview_delete_default_options() {
-	delete_option('preview_sites');
+	delete_option( 'preview_sites' );
 }
 
-require_once dirname( __FILE__ ) . '/src/settings.php';
+require_once dirname( __FILE__ ) . '/src/class-decoupled-preview-settings.php';
 
 new Decoupled_Preview_Settings();
 
-add_action('updated_option', function( $option_name, $option_value ) {
-	if ( $option_name === 'preview_sites' ) {
-		echo '<script type="text/javascript">window.location = "options-general.php?page=preview_sites"</script>';
-		exit;
-	}
-}, 10, 2);
+add_action(
+	'updated_option',
+	function( $option_name, $option_value ) {
+		if ( $option_name === 'preview_sites' ) {
+			echo '<script type="text/javascript">window.location = "options-general.php?page=preview_sites"</script>';
+			exit;
+		}
+	},
+	10,
+	2
+);
