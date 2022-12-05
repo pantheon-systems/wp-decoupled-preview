971886d97c23437c3a8a1b55cce538c5177f6540
Brian Perry <brian.perry@pantheon.io>
2022-12-04 18:59:34 -0600

    DB-4072: coding standards fixes

--- js/add-new-preview-btn.js
+++ js/add-new-preview-btn.js
@@ -9,24 +9,23 @@
 window.addEventListener(
 	"load",
 	() => {
-    const previewBlock        = document.querySelector( ".block-editor-post-preview__dropdown" );
-    const decoupledPreviewBtn = document.getElementById( 'wp-admin-bar-decoupled-preview' );
-    // Remove the old Preview button.
+		const previewBlock    	  = document.querySelector( ".block-editor-post-preview__dropdown" );
+		const decoupledPreviewBtn = document.getElementById( 'wp-admin-bar-decoupled-preview' );
+		// Remove the old Preview button.
 		previewBlock.removeChild( previewBlock.querySelector( 'button' ) );
-    // Add Decoupled Preview Button into the same Preview continer.
+		// Add Decoupled Preview Button into the same Preview continer.
 		previewBlock.appendChild( decoupledPreviewBtn );
-    // Hide submenu items by default.
+		// Hide submenu items by default.
 		const previewSubmenu = decoupledPreviewBtn.querySelector( ".ab-sub-wrapper" );
-    previewSubmenu.classList.add( "hidden" )
-
+		previewSubmenu.classList.add( "hidden" );
 		// Add event listener to toggle submenu items.
 		decoupledPreviewBtn.addEventListener(
 			"click",
 			(e) => {
-        wp.data.dispatch('core/editor').autosave();
-        previewSubmenu.classList.toggle( "hidden" );
-        previewSubmenu.classList.toggle( "components-popover__content" );
-        // Change the edit post side bar z-index.
+				wp.data.dispatch( 'core/editor' ).autosave();
+				previewSubmenu.classList.toggle( "hidden" );
+				previewSubmenu.classList.toggle( "components-popover__content" );
+				// Change the edit post side bar z-index.
 				document.querySelector( '.interface-interface-skeleton__sidebar' ).classList.toggle( 'interface-z-index-0' );
 			}
 		);
--- templates/preview-template.php
+++ templates/preview-template.php
@@ -1,23 +1,26 @@
 <?php
+/**
+ * Assemble necessary params for preview API route
+ *
+ * @package wp-decoupled-preview
+ */
 
-// Assemble necessary params for preview API route
-
-$preview_id = sanitize_text_field( $_GET['preview_id'] );
-$preview_site_id = sanitize_text_field( $_GET['decoupled_preview_site'] );
-$nonce = sanitize_text_field( $_GET['preview_nonce'] );
+$preview_id      = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : false;
+$preview_site_id = isset( $_GET['decoupled_preview_site'] ) ? sanitize_text_field( wp_unslash( $_GET['decoupled_preview_site'] ) ) : false;
+$nonce           = isset( $_GET['preview_nonce'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_nonce'] ) ) : false;
 
 if ( ! wp_verify_nonce( $nonce, 'post_preview_' . $preview_id ) ) {
-    wp_die( 'Unable to preview: invalid nonce' );
+	wp_die( 'Unable to preview: invalid nonce' );
 }
 
-$post = get_post( $preview_id );
-$revision = wp_get_post_autosave( $preview_id, get_current_user_id() );
-$post_type = get_post_type($post);
+$preview_post      = get_post( $preview_id );
+$revision          = wp_get_post_autosave( $preview_id, get_current_user_id() );
+$preview_post_type = get_post_type( $preview_post );
 
 $preview_helper = new Decoupled_Preview_Settings();
-$preview_site = $preview_helper->get_preview_site($preview_site_id);
+$preview_site   = $preview_helper->get_preview_site( $preview_site_id );
 
-$redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri={$post->post_name}&id={$revision->ID}&content_type={$post_type}";
+$redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri={$preview_post->post_name}&id={$revision->ID}&content_type={$preview_post_type}";
 ?>
 
 <html lang="en">
@@ -30,17 +33,17 @@ $redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri=
 
 	<script>
 		<?php
-			if ( $preview_site['url'] ) {
-				// Redirecting via JS because the page headers have already been set by
-        // the time we get into this template so PHP wont redirect.
-				echo 'window.location.replace("'. $redirect .'");';
-			}
+		if ( $preview_site['url'] ) {
+			// Redirecting via JS because the page headers have already been set by
+			// the time we get into this template so PHP wont redirect.
+			echo 'window.location.replace("' . esc_url_raw( $redirect ) . '");';
+		}
 		?>
 	</script>
 </head>
 
 <body>
-  <h1>Redirecting to <?php echo $preview_site['label'] ?>...</h1>
+	<h1>Redirecting to preview site...</h1>
 </body>
 
 <?php wp_footer(); ?>
