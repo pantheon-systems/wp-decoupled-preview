a8043128e624ea0fe3cd413dfb466e602456ae4b
Chris Reynolds <chris@jazzsequence.com>
2023-03-10 09:37:10 -0700

    don't worry about aligning variables

--- templates/preview-template.php
+++ templates/preview-template.php
@@ -7,20 +7,20 @@
 
 namespace Pantheon\DecoupledPreview;
 
-$preview_id      = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : false;
+$preview_id = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : false;
 $preview_site_id = isset( $_GET['decoupled_preview_site'] ) ? sanitize_text_field( wp_unslash( $_GET['decoupled_preview_site'] ) ) : false;
-$nonce           = isset( $_GET['preview_nonce'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_nonce'] ) ) : false;
+$nonce = isset( $_GET['preview_nonce'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_nonce'] ) ) : false;
 
 if ( ! wp_verify_nonce( $nonce, 'post_preview_' . $preview_id ) ) {
 	wp_die( 'Unable to preview: invalid nonce' );
 }
 
-$preview_post      = get_post( $preview_id );
-$revision          = wp_get_post_autosave( $preview_id, get_current_user_id() );
+$preview_post = get_post( $preview_id );
+$revision = wp_get_post_autosave( $preview_id, get_current_user_id() );
 $preview_post_type = get_post_type( $preview_post );
 
 $preview_helper = new Decoupled_Preview_Settings();
-$preview_site   = $preview_helper->get_preview_site( $preview_site_id );
+$preview_site = $preview_helper->get_preview_site( $preview_site_id );
 
 $redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri={$preview_post->post_name}&id={$revision->ID}&content_type={$preview_post_type}";
 ?>
