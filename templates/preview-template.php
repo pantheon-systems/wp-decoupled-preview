161d708dff29b91b4d2085b75f2a7e9d2bcbf7f0
Chris Reynolds <chris@jazzsequence.com>
2023-03-10 09:59:03 -0700

    [CMSP-206] Audit the preview-template.php file (#18)
    * don't worry about aligning variables
    
    * add i18n
    
    * multi-line comments should use /* rather than //

--- templates/preview-template.php
+++ templates/preview-template.php
@@ -7,20 +7,20 @@
 
 namespace Pantheon\DecoupledPreview;
 
-$preview_id      = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : false;
+$preview_id = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : false;
 $preview_site_id = isset( $_GET['decoupled_preview_site'] ) ? sanitize_text_field( wp_unslash( $_GET['decoupled_preview_site'] ) ) : false;
-$nonce           = isset( $_GET['preview_nonce'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_nonce'] ) ) : false;
+$nonce = isset( $_GET['preview_nonce'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_nonce'] ) ) : false;
 
 if ( ! wp_verify_nonce( $nonce, 'post_preview_' . $preview_id ) ) {
 	wp_die( 'Unable to preview: invalid nonce' );
 }
 
-$preview_post      = get_post( $preview_id );
-$revision          = wp_get_post_autosave( $preview_id, get_current_user_id() );
+$preview_post = get_post( $preview_id );
+$revision = wp_get_post_autosave( $preview_id, get_current_user_id() );
 $preview_post_type = get_post_type( $preview_post );
 
 $preview_helper = new Decoupled_Preview_Settings();
-$preview_site   = $preview_helper->get_preview_site( $preview_site_id );
+$preview_site = $preview_helper->get_preview_site( $preview_site_id );
 
 $redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri={$preview_post->post_name}&id={$revision->ID}&content_type={$preview_post_type}";
 ?>
@@ -31,13 +31,15 @@ $redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri=
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Decoupled Preview</title>
+	<title><?php esc_html_e( 'Decoupled Preview', 'wp-decoupled-preview' ); ?></title>
 
 	<script>
 		<?php
 		if ( $preview_site['url'] ) {
-			// Redirecting via JS because the page headers have already been set by
-			// the time we get into this template so PHP wont redirect.
+			/*
+			 * Redirecting via JS because the page headers have already been
+			 * set by the time we get into this template so PHP won't redirect.
+			 */
 			echo 'window.location.replace("' . esc_url_raw( $redirect ) . '");';
 		}
 		?>
@@ -45,7 +47,7 @@ $redirect = "{$preview_site['url']}?secret={$preview_site['secret_string']}&uri=
 </head>
 
 <body>
-	<h1>Redirecting to preview site...</h1>
+	<h1><?php esc_html_e( 'Redirecting to preview site...', 'wp-decoupled-preview' ); ?></h1>
 </body>
 
 <?php wp_footer(); ?>
