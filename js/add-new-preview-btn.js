8a38538645334c8cf1d4d82c293fdc54c0654422
Brian Perry <brian.perry@pantheon.io>
2023-06-13 12:05:40 -0500

    DB-6162: refactor js to ensure preview button has loaded

--- js/add-new-preview-btn.js
+++ js/add-new-preview-btn.js
@@ -9,25 +9,41 @@
 window.addEventListener(
 	"load",
 	() => {
-		const previewBlock    	  = document.querySelector( ".block-editor-post-preview__dropdown" );
-		const decoupledPreviewBtn = document.getElementById( 'wp-admin-bar-decoupled-preview' );
-		// Remove the old Preview button.
-		previewBlock.removeChild( previewBlock.querySelector( 'button' ) );
-		// Add Decoupled Preview Button into the same Preview continer.
-		previewBlock.appendChild( decoupledPreviewBtn );
-		// Hide submenu items by default.
-		const previewSubmenu = decoupledPreviewBtn.querySelector( ".ab-sub-wrapper" );
-		previewSubmenu.classList.add( "hidden" );
-		// Add event listener to toggle submenu items.
-		decoupledPreviewBtn.addEventListener(
-			"click",
-			(e) => {
-				wp.data.dispatch( 'core/editor' ).autosave();
-				previewSubmenu.classList.toggle( "hidden" );
-				previewSubmenu.classList.toggle( "components-popover__content" );
-				// Change the edit post side bar z-index.
-				document.querySelector( '.interface-interface-skeleton__sidebar' ).classList.toggle( 'interface-z-index-0' );
-			}
-		);
+    let editorChecks = 0;
+
+    // Ensure that block editor preview button exists, and if so, modify it.
+    const checkPreview = () => {
+      const previewBlock    	  = document.querySelector( ".block-editor-post-preview__dropdown" );
+      const decoupledPreviewBtn = document.getElementById( 'wp-admin-bar-decoupled-preview' );
+      // Remove the old Preview button.
+      if (previewBlock) {
+        previewBlock.removeChild( previewBlock.querySelector( 'button' ) );
+        // Add Decoupled Preview Button into the same Preview container.
+        previewBlock.appendChild( decoupledPreviewBtn );
+        // Hide submenu items by default.
+        const previewSubmenu = decoupledPreviewBtn.querySelector( ".ab-sub-wrapper" );
+        previewSubmenu.classList.add( "hidden" );
+        // Add event listener to toggle submenu items.
+        decoupledPreviewBtn.addEventListener(
+          "click",
+          (e) => {
+            wp.data.dispatch( 'core/editor' ).autosave();
+            previewSubmenu.classList.toggle( "hidden" );
+            previewSubmenu.classList.toggle( "components-popover__content" );
+            // Change the edit post side bar z-index.
+            document.querySelector( '.interface-interface-skeleton__sidebar' ).classList.toggle( 'interface-z-index-0' );
+          }
+        );
+        clearInterval(checkPreviewInterval);
+      }
+      // Limit the number of checks for the preview button.
+      else {
+        editorChecks++;
+        if (editorChecks > 24) {
+          clearInterval(checkPreviewInterval);
+        }
+      }
+    }
+    const checkPreviewInterval = setInterval(checkPreview, 200);
 	}
 );
